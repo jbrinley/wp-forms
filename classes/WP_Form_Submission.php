83f3711452edf587e22c8f0a4647397f16471eed
Jonathan Brinley <jbrinley@adeliedesign.com>
2013-01-29 10:40:58 -0500

    pass submission object to submit callbacks

--- classes/WP_Form_Submission.php
+++ classes/WP_Form_Submission.php
@@ -18,15 +18,36 @@ class WP_Form_Submission {
 		return empty($errors);
 	}
 
+	public function get_errors(){
+		return $this->errors;
+	}
+
 	public function submit() {
 		$processors = $this->form->get_processors();
 		foreach ( $processors as $callback ) {
-			call_user_func( $callback, $this->data, $this->form );
+			call_user_func( $callback, $this, $this->form );
 		}
 	}
 
-	public function get_errors(){
-		return $this->errors;
+	/**
+	 * @param string $key The form element name, as it would be displayed
+	 * in the HTML.
+	 *
+	 * @return string|array|null The submitted value, or NULL if $key can't be found
+	 */
+	public function get_value( $key ) {
+		$components = $this->parse_key($key);
+		if ( empty($components) ) {
+			return NULL; // invalid key
+		}
+		$data = $this->data;
+		while ( ( $key = array_shift($components) ) !== NULL ) {
+			if ( !is_array($data) || !isset($data[$key]) ) {
+				return NULL; // key not found
+			}
+			$data = $data[$key]; // move down the array
+		}
+		return $data;
 	}
 
 	protected function validate() {
@@ -39,5 +60,17 @@ class WP_Form_Submission {
 		foreach ( $validators as $callback ) {
 			call_user_func( $callback, $this->data, $this->errors, $this->form );
 		}
+	}
+
+	/**
+	 * @param string $key
+	 * @return array
+	 */
+	protected function parse_key( $key ) {
+		$components = explode('[', $key);
+		foreach ( $components as &$c ) {
+			$c = rtrim($c, ']');
+		}
+		return $components;
 	}
 }
