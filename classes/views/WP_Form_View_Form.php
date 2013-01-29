8900b589ed0bf7e7f8a068214d4b7eedf8b1e1d8
Jonathan Brinley <jbrinley@adeliedesign.com>
2013-01-29 09:51:16 -0500

    remove that walker nonsense. just iterate

--- classes/views/WP_Form_View_Form.php
+++ classes/views/WP_Form_View_Form.php
@@ -1,16 +1,14 @@
 <?php
 
 class WP_Form_View_Form extends WP_Form_View {
-	protected $children = '';
 
 	public function form( WP_Form $form ) {
-		$this->render_children($form);
+		$children = $this->render_children($form);
 		$output = sprintf(
 			'<form %s>%s</form>',
 			WP_Form_View::prepare_attributes($form->get_all_attributes()),
-			$this->children
+			$children
 		);
-		$this->children = ''; // clean up our mess
 		return $output;
 	}
 
@@ -18,21 +16,21 @@ class WP_Form_View_Form extends WP_Form_View {
 	 * Walk through each child and render it
 	 *
 	 * @param WP_Form $form
+	 * @return string
 	 */
 	protected function render_children( WP_Form $form ) {
-		$walker = new WP_Form_Walker( $form );
-		$walker->walk( array( $this, 'render_child' ) );
+		$children = '';
+		foreach ( $form->get_children() as $child ) {
+			$children = $this->render_child($child);
+		}
+		return $children;
 	}
 
 	/**
-	 * Callback for the walker.
-	 *
-	 * @access private
-	 * @see WP_Form_Walker::walk()
 	 * @param WP_Form_Component $element
 	 */
-	public function render_child( WP_Form_Component $element ) {
-		$this->children .= $element->render();
+	protected function render_child( WP_Form_Component $element ) {
+		return $element->render();
 	}
 
 }
