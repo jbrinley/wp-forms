d2e90328ecd650ad4a3b58b96255fe76b33ac5cf
Jonathan Brinley <jbrinley@adeliedesign.com>
2013-02-06 15:16:03 -0500

    add 'selected' attributed to selected option in a select element

--- classes/elements/WP_Form_Element_Select.php
+++ classes/elements/WP_Form_Element_Select.php
@@ -3,5 +3,21 @@
 class WP_Form_Element_Select extends WP_Form_Element_Multiple {
 	protected $type = 'select';
 	protected $default_view = 'WP_Form_View_Select';
-	// TODO: optgroup
+
+
+	/**
+	 * Don't need the extra attributes of an input element
+	 * @return array
+	 */
+	public function get_all_attributes() {
+		$attributes = $this->attributes->get_all_attributes();
+		return $attributes;
+	}
+
+	public function get_selected() {
+		if ( !empty($this->value) ) {
+			return $this->value;
+		}
+		return $this->default_value;
+	}
 }
--- classes/views/WP_Form_View_Select.php
+++ classes/views/WP_Form_View_Select.php
@@ -8,28 +8,29 @@ class WP_Form_View_Select extends WP_Form_View {
 		$template = '<select %s>%s</select>';
 		$options = $element->get_options();
 		$options_html = '';
+		$selected = $element->get_selected();
 		foreach ( $options as $key => $value ) {
 			if ( is_array($value) ) {
-				$options_html .= $this->optgroup( $key, $value );
+				$options_html .= $this->optgroup( $key, $value, $selected );
 			} else {
-				$options_html .= $this->option( $key, $value );
+				$options_html .= $this->option( $key, $value, $selected );
 			}
 		}
 		return sprintf( $template, $attributes, $options_html );
 	}
 
-	protected function optgroup( $label, $options ) {
+	protected function optgroup( $label, $options, $selected ) {
 		$html = sprintf( '<optgroup label="%s">', esc_attr($label) );
 		foreach ( $options as $key => $value ) {
-			$html .= $this->option($key, $value);
+			$html .= $this->option( $key, $value, $selected );
 		}
 		$html .= '</optgroup>';
 		return $html;
 	}
 
-	protected function option( $key, $label ) {
-		$option_template = '<option value="%s">%s</option>';
-		return sprintf($option_template, esc_attr($key), esc_html($label));
+	protected function option( $key, $label, $selected ) {
+		$option_template = '<option value="%s"%s>%s</option>';
+		return sprintf($option_template, esc_attr($key), selected($selected, $key, FALSE), esc_html($label));
 	}
 
 }
