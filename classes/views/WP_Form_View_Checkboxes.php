5530e1fb1002aeb66c8b44e7f7c101d46dbb7b5a
Richard Sweeney <theorobman@gmail.com>
2014-01-18 13:08:12 +0100

    Typecast all values as an array, in case there is a string passed as the default value.

--- classes/views/WP_Form_View_Checkboxes.php
+++ classes/views/WP_Form_View_Checkboxes.php
@@ -7,11 +7,11 @@ class WP_Form_View_Checkboxes extends WP_Form_View {
 			throw new LogicException(__('Cannot render checkbox group without a name', 'wp-forms'));
 		}
 		$options = $element->get_options();
-		$selected = $element->get_selected();
+		$selected = (array) $element->get_selected();
 		$output = '';
 		foreach ( $options as $key => $label ) {
 			$checked = false;
-			if ( is_array( $selected ) && in_array( $key, $selected ) ) {
+			if ( in_array( $key, $selected ) ) {
 				$checked = true;
 			}
 			$output .= $this->checkbox( $key, $label, $attributes, $checked );
