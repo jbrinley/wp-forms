f4e3b2d0018e5a3d36f5464979fc812d4301fb7b
Kolya Korobochkin <me@korobochkin.com>
2015-08-01 23:40:27 +0300

    Проблема с несохраняемыми чекбоксами решена с помощью коммита из другого форка https://github.com/jbrinley/wp-forms/issues/8

--- classes/elements/WP_Form_Element_Checkboxes.php
+++ classes/elements/WP_Form_Element_Checkboxes.php
@@ -3,4 +3,11 @@
 class WP_Form_Element_Checkboxes extends WP_Form_Element_Multiple {
 	protected $type = 'checkboxes';
 	protected $default_view = 'WP_Form_View_Checkboxes';
+
+	public function get_selected() {
+		if ( !empty($this->value) ) {
+			return $this->value;
+		}
+		return $this->default_value;
+	}
 }
